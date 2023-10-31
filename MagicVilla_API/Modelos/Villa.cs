e7de46b355e6f87f53c410d52826a52efe5a8f8e
Juanjobe <juanjobe@gmail.com>
2023-10-31 00:23:00 -0300

    subiendo modificaciones

--- MagicVilla_API/Modelos/Villa.cs
+++ MagicVilla_API/Modelos/Villa.cs
@@ -8,14 +8,19 @@ namespace MagicVilla_API.Modelos
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Detalle { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? Nombre { get; set; }
+        [Required]
+        public string? Detalle { get; set; }
         [Required] 
         public double Tarifa { get; set; }
         public int  Ocupantes { get; set; }
         public int Metros2 {  get; set; }
-        public string ImagenUrl { get; set; }
-        public string Amenidad { get; set; }
+        [Required]
+        public string? ImagenUrl { get; set; }
+        [Required]
+        public string? Amenidad { get; set; }
         public DateTime FechaCreacion { get; set; }
         public DateTime FechaActualizacion { get; set; }
     }
